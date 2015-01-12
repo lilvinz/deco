5024e9fa9cea2fe768d494da59c4e59e5cd47953
lilvinz <lilvinz@nscit.com>
2015-01-12 21:08:00 +0100

    cleanup

--- deco.py
+++ deco.py
@@ -60,28 +60,27 @@ class DecoZHL16:
             * (1 - numpy.power(2, -1 * time / self.halftime_he))
         
         # Update weighted a and b values.
-        if (self.p_tissues_n2.all() + self.p_tissues_he.all()) != 0:
-            self.a = (self.a_n2 * self.p_tissues_n2 + self.a_he * self.p_tissues_he) \
-                / (self.p_tissues_n2 + self.p_tissues_he);
-            self.b = (self.b_n2 * self.p_tissues_n2 + self.b_he * self.p_tissues_he) \
-                / (self.p_tissues_n2 + self.p_tissues_he);
-            
-    def update_asc_desc(self, time, p_start, p_end, f_n2, f_he):
-        p_start_alv_n2 = self.dtype((p_start - self.p_h2o) * f_n2)
-        p_start_alv_he = self.dtype((p_start - self.p_h2o) * f_he)
-        R_n2 = self.dtype(((p_end - self.p_h2o) * f_n2 - p_start_alv_n2) / time)
-        R_he = self.dtype(((p_end - self.p_h2o) * f_he - p_start_alv_he) / time)
+        self.a = (self.a_n2 * self.p_tissues_n2 + self.a_he * self.p_tissues_he) \
+            / (self.p_tissues_n2 + self.p_tissues_he);
+        self.b = (self.b_n2 * self.p_tissues_n2 + self.b_he * self.p_tissues_he) \
+            / (self.p_tissues_n2 + self.p_tissues_he);
+
+    def update_asc_desc(self, time, p_begin, p_end, f_n2, f_he):
+        p_begin_alv_n2 = self.dtype((p_begin - self.p_h2o) * f_n2)
+        p_begin_alv_he = self.dtype((p_begin - self.p_h2o) * f_he)
+        R_n2 = self.dtype(((p_end - self.p_h2o) * f_n2 - p_begin_alv_n2) / time)
+        R_he = self.dtype(((p_end - self.p_h2o) * f_he - p_begin_alv_he) / time)
         
         for i in range(0, self.n_tissues):
             # Update tissues gas loadings
             k = self.dtype(numpy.log(2) / self.halftime_n2[i])
-            self.p_tissues_n2[i] = p_start_alv_n2 + R_n2 * (time - 1 / k) \
-                - (p_start_alv_n2 - self.p_tissues_n2[i] - (R_n2 / k)) \
+            self.p_tissues_n2[i] = p_begin_alv_n2 + R_n2 * (time - 1 / k) \
+                - (p_begin_alv_n2 - self.p_tissues_n2[i] - (R_n2 / k)) \
                 * numpy.exp(-k * time)
             
             k = self.dtype(numpy.log(2) / self.halftime_he[i])
-            self.p_tissues_he[i] = p_start_alv_he + R_he * (time - 1 / k) \
-                - (p_start_alv_he - self.p_tissues_he[i] - (R_he / k)) \
+            self.p_tissues_he[i] = p_begin_alv_he + R_he * (time - 1 / k) \
+                - (p_begin_alv_he - self.p_tissues_he[i] - (R_he / k)) \
                 * numpy.exp(-k * time)
             
             # Update the weighted a and b values
@@ -92,16 +91,7 @@ class DecoZHL16:
                     / (self.p_tissues_n2[i] + self.p_tissues_he[i])
             
     def ceiling_get_pressure(self, gf):
-        #return (self.p_tissues_n2 + self.p_tissues_he - self.a * gf) / (gf / self.b - gf + 1).max()
-        ceiling = 0
-        for i in range(0, self.n_tissues):
-            this_ceiling = self.dtype((self.p_tissues_n2[i] + self.p_tissues_he[i] - self.a[i] * gf)
-                / (gf / self.b[i] - gf + 1))
-                
-            if this_ceiling > ceiling:
-                ceiling = this_ceiling
-                
-        return ceiling;
+        return ((self.p_tissues_n2 + self.p_tissues_he - self.a * gf) / (gf / self.b - gf + 1)).max()
     
     def depth_to_pressure(self, depth):
         return self.p_surface + depth * 1.0 * 1.0 * 0.1
@@ -112,19 +102,11 @@ class DecoZHL16:
         return (p_ambient - self.p_surface) / 1.0 / 1.0 / 0.1
     
     def gf_current_get(self, p_ambient):
-        #return (self.p_tissues_n2 + self.p_tissues_he - p_ambient) / (((p_ambient / self.b + self.a) - p_ambient)).max()
-        
-        max_gf = 0
-        
-        for i in range(0, self.n_tissues):
-            this_gf = self.dtype((self.p_tissues_n2[i] + self.p_tissues_he[i] - p_ambient)
-                / ((p_ambient / self.b[i] + self.a[i]) - p_ambient))
-                
-            if this_gf > max_gf:
-                max_gf = this_gf
-                
-        return max_gf;
-    
+        gf = ((self.p_tissues_n2 + self.p_tissues_he - p_ambient) / ((p_ambient / self.b + self.a) - p_ambient)).max()
+        if (gf < 0):
+            return 0
+        return gf
+
     def gf_allowed_get(self, p_ambient):
         if (p_ambient <= self.p_surface):
             return self.gf_high
@@ -160,18 +142,18 @@ class DecoZHL16:
         stop_time = 0
         
         # Determine first ceiling pressure.
-        _p_ceiling = self.dtype(temp_ctx.ceiling_get_pressure(self.gf_low))
+        p_ceiling = self.dtype(temp_ctx.ceiling_get_pressure(self.gf_low))
         
         # Determine lowest depth where no stop is required (worst-case).
         # This is where we will start looking for required stops.
-        stop_depth = numpy.ceil(self.pressure_to_depth(_p_ceiling))
+        stop_depth = numpy.ceil(self.pressure_to_depth(p_ceiling))
         
         # First stop candidate is the first higher depth on the 3m raster.
-        if stop_depth % self.stop_size:
+        if (stop_depth % self.stop_size):
             stop_depth = stop_depth - (stop_depth % self.stop_size) + self.stop_size
         
         # Raise pressure of GF_LOW to the first stop depth.
-        if (self.depth_to_pressure(stop_depth) > self.p_gf_low + 0.001):
+        if (self.depth_to_pressure(stop_depth) > self.p_gf_low):
             self.p_gf_low = self.depth_to_pressure(stop_depth)
         # Lower pressure of GF_LOW in case the diver is still below deco zone.
         #elif (self.depth_to_pressure(stop_depth) < self.p_gf_low - 0.001 and
@@ -181,12 +163,11 @@ class DecoZHL16:
         # If we are deeper than the first stop candidate, simulate ascent accordingly.
         if (p_ambient > self.depth_to_pressure(stop_depth)):
             
-            time = (self.pressure_to_depth(p_ambient) - stop_depth) \
-                / self.ascend_speed
+            time = (self.pressure_to_depth(p_ambient) - stop_depth) / self.ascend_speed
             
             temp_ctx.update_asc_desc(
                 time = time,
-                p_start = p_ambient,
+                p_begin = p_ambient,
                 p_end = self.depth_to_pressure(stop_depth),
                 f_n2 = f_n2,
                 f_he = f_he)
@@ -220,8 +201,8 @@ class DecoZHL16:
                     # No stop required so do the minimum stop of one minute
                     # but take ascent time from last stop into account. */
                     temp_ctx.update_constant(
-                        1 - self.stop_size / self.ascend_speed,
-                        self.depth_to_pressure(stop_depth),
+                        time = 1 - self.stop_size / self.ascend_speed,
+                        p_ambient = self.depth_to_pressure(stop_depth),
                         f_n2 = f_n2,
                         f_he = f_he)
                     
@@ -237,9 +218,9 @@ class DecoZHL16:
                 
                 # Simulate ascent to next stop.
                 temp_ctx.update_asc_desc(
-                    (stop_depth - clearance_depth) / self.ascend_speed,
-                    self.depth_to_pressure(stop_depth),
-                    self.depth_to_pressure(clearance_depth),
+                    time = (stop_depth - clearance_depth) / self.ascend_speed,
+                    p_begin = self.depth_to_pressure(stop_depth),
+                    p_end = self.depth_to_pressure(clearance_depth),
                     f_n2 = f_n2,
                     f_he = f_he)
                 
@@ -255,16 +236,16 @@ class DecoZHL16:
                     # This is the first waiting segment on this stop
                     # so subtract the ascent time from last stop.
                     temp_ctx.update_constant(
-                        1.0 - self.stop_size / self.ascend_speed,
-                        self.depth_to_pressure(stop_depth),
+                        time = 1 - self.stop_size / self.ascend_speed,
+                        p_ambient = self.depth_to_pressure(stop_depth),
                         f_n2 = f_n2,
                         f_he = f_he)
                 else:
                     # This is not the first waiting segment on this stop
                     # so do the full minute.
                     temp_ctx.update_constant(
-                        1.0,
-                        self.depth_to_pressure(stop_depth),
+                        time = 1,
+                        p_ambient = self.depth_to_pressure(stop_depth),
                         f_n2 = f_n2,
                         f_he = f_he)
                     
@@ -273,12 +254,11 @@ class DecoZHL16:
         # Finally add ascent time up to first stop to the TTS value.
         if (p_ambient > self.depth_to_pressure(self.stops[0]['depth'])):
             # Calculate time for ascent.
-            time = \
-                (self.pressure_to_depth(p_ambient) - self.stops[0]['depth']) \
+            time = (self.pressure_to_depth(p_ambient) - self.stops[0]['depth']) \
                 / self.ascend_speed
             
             # Add initial ascent from ambient to first stop.
-            self.time_to_surface += int(time + 0.999)
+            self.time_to_surface += numpy.ceil(time)
 
 class DecoZHL16B(DecoZHL16):
     # Constants
